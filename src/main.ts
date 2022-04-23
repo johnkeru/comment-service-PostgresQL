724c9989b768ef70f9ee6b16494b8b9265d4c657
keru <keruk355@gmail.com>
2022-04-23 18:37:27 +0800

    final move

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,23 @@ import {Logger} from '@nestjs/common';
 const logger = new Logger('Comment 🍲');
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.RMQ,
+  const isprod = process.env.PROD ? await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport:  Transport.RMQ,
       options: {
-        urls: ['amqps://eoayjqkg:pHq77ZZcEV1f08J67xO1HHNGfSGTcCbB@shrimp.rmq.cloudamqp.com/eoayjqkg'],
+        urls: [process.env.RABBIT_MQ_URL || 'amqps://eoayjqkg:pHq77ZZcEV1f08J67xO1HHNGfSGTcCbB@shrimp.rmq.cloudamqp.com/eoayjqkg'],
         queue: 'comment_queue',
         queueOptions: {
           durable: false
         },
       },
+  }) : await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport:  Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 9000
+      },
   })
+  const app = isprod
   app.listen()
   logger.log('🚀 Comment service is on fire🔥');
 }
